4c0857825ec03c9b97d37bbd2bf5ecce6ef4d4db
Rasikh Ali <rasikhali1234@gmail.com>
2026-03-08 01:28:35 +0500

    Update: for spring-26

--- app.py
+++ app.py
@@ -661,5 +661,9 @@ def download_section_by_semester(semester):
         download_name=f'semester_{semester}_timetable.xlsx'
     )
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
